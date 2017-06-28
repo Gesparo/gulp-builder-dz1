29b62b5b3f87673ab31a45f9ee3a8a8a0064c16a
Melnik Maksim <shmaxer@rambler.ru>
2017-06-28 15:36:40 +0500

    Add simple policy in main content confirm form

--- source/js/app.js
+++ source/js/app.js
@@ -3,7 +3,16 @@
 
   test.init();
 
+  // show policy
   $('.footer_policy').on('click', function (e) {
+    e.preventDefault();
+
+    moreInfoWindow.init();
+    moreInfoWindow.load('policy');
+  });
+
+  // show policy
+  $('.main-content__callback-policy-link').on('click', function (e) {
     e.preventDefault();
 
     moreInfoWindow.init();
